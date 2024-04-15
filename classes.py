3674881f77f583adafcea33988e3d5f7f28c86fa
Salma Tarek <salmatrna@gmail.com>
2024-04-15 11:15:02 +0200

    First time

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Employee:
+    grade = "Very Good"
+    def __init__(self, first, last, salary):
+        self.FirstName = first
+        self.LastName = last
+        self.Salary = salary
+        self.Email = first+'.'+last+'@company.com'
+
+    def profit(self,years):
+        netpay = self.Salary*int(years)
+        return netpay
+
+khamis = Employee("Mohamed", "Khamis", "40k USD")
+print(khamis.Email)
+sofy = Employee("Ahmed","Sofy",50000)
+share = sofy.profit(3)
+print(share)
